2eb8b51b84c9396af07aebcb2eca6e090d10c063
Yesica <yesicasunol@gmail.com>
2022-09-16 18:35:04 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ComponentsComponent } from './components/components.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { StudentComponent } from './components/student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ComponentsComponent,
+    ToolbarComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
