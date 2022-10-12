931469ef11bc8ad547a919b7ea2141c50e497813
Yesica <yesicasunol@gmail.com>
2022-10-12 13:45:23 -0300

    issues

--- ComponentesYesicaSuñol/src/app/app-routing.module.ts
+++ ComponentesYesicaSuñol/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearAlumnosComponent } from './components/crear-alumnos/crear-alumnos.component';
+import { ListaAlumnosComponent } from './components/lista-alumnos/lista-alumnos.component';
+import { StudentComponent } from './components/student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'estudiantes', pathMatch: 'full'},
+  { path: 'estudiantes', component: StudentComponent},
+  { path: 'lista-alumnos', component: ListaAlumnosComponent},
+  { path: 'crear-alumnos', component: CrearAlumnosComponent},
+  { path: '**', redirectTo: 'estudiantes', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ComponentesYesicaSuñol/src/app/app.module.ts
+++ ComponentesYesicaSuñol/src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//MODULOS
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+import { ReactiveFormsModule } from '@angular/forms';
+import  {  ToastrModule  }  from  'ngx-toastr' ;
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//COMPONENTES
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ComponentsComponent } from './components/components.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { StudentComponent } from './components/student/student.component';
+import { ListaAlumnosComponent } from './components/lista-alumnos/lista-alumnos.component';
+import { CrearAlumnosComponent } from './components/crear-alumnos/crear-alumnos.component';
+
+
 
 @NgModule({
   declarations: [
@@ -14,11 +28,19 @@ import { StudentComponent } from './components/student/student.component';
     NavbarComponent,
     ComponentsComponent,
     ToolbarComponent,
-    StudentComponent
+    StudentComponent,
+    ListaAlumnosComponent,
+    CrearAlumnosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAnalyticsModule,
+    ReactiveFormsModule,
+    ToastrModule ,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
